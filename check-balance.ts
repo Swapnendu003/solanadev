f8d9ef07a0822625e65b2a1796c131c109bf9953
Manice18 <manishmanice2003@gmail.com>
2024-04-11 21:27:06 +0530

    fix: keypair generate

--- check-balance.ts
+++ check-balance.ts
@@ -3,7 +3,10 @@ import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 async function getBalance(address) {
   try {
     const publicKey = new PublicKey(address);
-    const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+    const connection = new Connection(
+      "https://api.mainnet-beta.solana.com",
+      "confirmed"
+    );
     const balanceInLamports = await connection.getBalance(publicKey);
     const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
     console.log(
--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,5 +1,14 @@
+import fs from "fs";
 import { Keypair } from "@solana/web3.js";
 const keypair = Keypair.generate();
+fs.appendFile(
+  "keypair.txt",
+  `[${keypair.secretKey.toString()}]`,
+  function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  }
+);
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
 console.log(`✅ Finished!`);
